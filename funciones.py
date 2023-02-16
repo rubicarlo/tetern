a9228e92e5ccdf7e2885ea6ba791f5b5affd85d0
rubicarlo <rubicarlo.95@hotmail.com>
2023-02-16 10:37:54 +0100

    prueba

---
+++ funciones.py
@@ -0,0 +1,15 @@
+import subprocess
+
+processes = []
+
+def download_video(url, index):
+    process = subprocess.Popen(['ffmpeg', '-loglevel', 'panic', '-i', url, f'video_{index}.ts'])
+    processes.append(process)
+
+def stop_download(index):
+    process = processes[index]
+    if process is not None:
+        process.terminate()
+        process.wait()
+        processes[index] = None
+        print(f"La descarga del video {index} ha sido detenida.")
