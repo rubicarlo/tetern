2b03dc1590105cf88435e6447d519d1e92588414
rubicarlo <rubicarlo.95@hotmail.com>
2023-02-20 14:48:16 +0100

    aversisi

--- funciones.py
+++ funciones.py
@@ -3,7 +3,7 @@ import subprocess
 processes = []
 
 def download_video(url, index):
-    process = subprocess.Popen(['ffmpeg', '-loglevel', 'panic', '-i', url, f'video_{index}.ts'])
+    process = subprocess.Popen(['ffmpeg', '-loglevel', 'panic', '-i', url, f'video_{index}.mp4'])
     processes.append(process)
 
 def stop_download(index):
