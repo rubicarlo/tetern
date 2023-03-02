fc22000c237025a38df117aa4d5870eee43b8737
rubicarlo <rubicarlo.95@hotmail.com>
2023-03-02 11:07:08 +0100

    reto

--- funciones.py
+++ funciones.py
@@ -1,11 +1,20 @@
 import subprocess
+import os
+
 
 processes = []
 
 def download_video(url, index):
-    process = subprocess.Popen(['ffmpeg', '-loglevel', 'panic', '-i', url, '-b:v', '2M', f'video_{index}.ts'])
-
+    if not os.path.exists('video'):
+        os.makedirs('video')
+    process = subprocess.Popen(['ffmpeg', '-loglevel', 'panic', '-i', url, '-c', 'copy', '-b:v', '5M', os.path.join('video', f'video_{index}.ts')])
     processes.append(process)
+
+def convert_to_mp4(index):
+    if not os.path.exists(os.path.join('video', f'video_{index}.ts')):
+        return
+    subprocess.run(['ffmpeg', '-loglevel', 'panic', '-i', os.path.join('video', f'video_{index}.ts'), '-c', 'copy', os.path.join('video', f'video_{index}.mp4')])
+    os.remove(os.path.join('video', f'video_{index}.ts'))
 
 def stop_download(index):
     process = processes[index]
@@ -13,4 +22,5 @@ def stop_download(index):
         process.terminate()
         process.wait()
         processes[index] = None
-        print(f"La descarga del video {index} ha sido detenida.")
+    convert_to_mp4(index)
+    print(f"La descarga del video {index} ha sido detenida y convertida a un archivo mp4.")
